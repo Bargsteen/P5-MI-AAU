01b6d1515603e81703725b135a80de61ae9a587b
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2017-10-17 14:36:56 +0200

    Order simplified

--- scrapers/Classes.py
+++ scrapers/Classes.py
@@ -7,20 +7,22 @@ def checkTypeReturn(input, expectedType):
     else:
         raise TypeError(str(input) + " is of type " + str(type(input)) + " expected: " + str(expectedType))
 
+
 class ArticleNumber:
     def __init__(self, articleNum : int):
         self.articleNumber = checkTypeReturn(articleNum, int)
         if 10000000 > self.articleNumber or self.articleNumber > 9999999999:
             raise ValueError("Invalid article number entered.")
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.articleNumber)
+
 
 class ItemType:
     totalProcessTime = datetime.time
     def __init__(self, articleNum: ArticleNumber):
         self.articleNumber = checkTypeReturn(articleNum, ArticleNumber)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.articleNumber)
 
 
@@ -29,16 +31,28 @@ class Line:
         self.itemType = checkTypeReturn(itemType, ItemType)
         self.count = checkTypeReturn(count, int)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "(%s, %s)" % (str(self.itemType), str(self.count))
 
-art = ArticleNumber(232323232)
-itemTy = ItemType(art)
-line = Line(itemTy, 1234)
 
-print(line)
+class Order:
+    def __init__(self, orderName : str, timeToFinish : int):
+        self.orderName = orderName
+        self.timeToFinish = timeToFinish
 
-#class Order:
- #   timeOfReceival = datetime.datetime
-   # orderId = ""
-  #  lines = [(ItemType(), 0)]
+    def __str__(self) -> str:
+        return "(%s, %s)" % (self.orderName, self.timeToFinish)
+
+
+
+
+
+
+myOrder = Order("hej2", 321)
+print(myOrder.__str__())
+
+
+
+
+
+
