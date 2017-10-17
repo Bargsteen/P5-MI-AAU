bdee5851a0945be79484f8dc9ddef84c9d317a8b
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2017-10-17 15:22:03 +0200

    added system

--- scrapers/Classes.py
+++ scrapers/Classes.py
@@ -43,13 +43,47 @@ class Order:
     def __str__(self) -> str:
         return "(%s, %s)" % (self.orderName, self.timeToFinish)
 
+class OrderBox:
+    def __init__(self, order : Order ):
+        self.order = order
+        self.timeRemaining = order.timeToFinish
+
+    def __str__(self):
+        return "(%s, %s)" % (self.order, self.timeRemaining)
 
 
+class System:
+    def __init__(self, orderList, maxConcurrentItems : int):
+        self.orderList = orderList
+        self.finishedOrders = [Order]
+        self.maxConcurrentItems = maxConcurrentItems
+        self.orderBoxList = [OrderBox]
+
+    def initializeOrderBoxes(self):
+        # Lav en orderboxlist
+        self.orderBoxList = [OrderBox]
+
+        # for hver ordre i orderlist:
+        for order in self.orderList:
+            # opret ny orderbox med order og tid fra ordren
+            newOrderBox = OrderBox(order)
+
+            # tilføj den nye orderbox til orderboxlist
+            self.orderBoxList.append(newOrderBox)
+
+    def __str__(self):
+        myStr = ("My Order Boxes:\n")
+        for order in self.orderBoxList:
+            myStr += str(order) + "\n"
+        return myStr
 
 
+myOrders = [Order("A", 1), Order("B", 21), Order("C", 12), Order("D", 41)]
 
-myOrder = Order("hej2", 321)
-print(myOrder.__str__())
+system = System(myOrders, 3)
+system.initializeOrderBoxes()
+
+print(system)
 
 
 
