9d8e07bc723bb221faee9c619763dfbd997c0482
cakn15 <cakn15@student.aau.dk>
2017-10-20 10:14:45 +0200

    Test classes for Picking and Erp

--- SolarSystem/SolarSystem/Picking and ERP/PickingScrape.cs
+++ SolarSystem/SolarSystem/Picking and ERP/PickingScrape.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SolarSystem.Picking_and_ERP
 {
     class PickingScrape
     {
+        public PickingScrape(string Path)
+        {
+            _path = Path;
+        }
+
+        static string _path;
+
+
+
+        var reader = new StreamReader(@_path);
     }
 }
