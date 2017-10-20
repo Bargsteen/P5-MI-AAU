cf77418ca03ad77b04545d7bf067480cf206c63d
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2017-10-20 09:08:16 +0200

    Theorys added as an example for xunit

--- SolarSystem/SolarSystem.Test/Tests.cs
+++ SolarSystem/SolarSystem.Test/Tests.cs
@@ -13,7 +13,7 @@ namespace SolarSystem.Test
         }
 
         [Fact]
-        public void Add_TwoPlusTwo_ReturnsFour()
+        public void SolarAdd_TwoPlusTwo_ReturnsFour()
         {
             // Arrange
             int num1 = 2;
@@ -25,5 +25,23 @@ namespace SolarSystem.Test
             // Assert
             Assert.Equal(result, 4);
         }
+
+        [Theory]
+        [InlineData(3, 2, 5)]
+        [InlineData(2, 2, 4)]
+        [InlineData(-10, -10, -20)]
+        public void SolarAdd_AdditionOfTwoNumbers_ReturnsCorrectResult(int num1, int num2, int expected)
+        {
+            // Arrange
+            // as input
+            
+            // Act
+            int res = Program.SolarAdd(num1, num2);
+            
+            // Assert
+            Assert.Equal(res, expected);
+        }
+        
+        
     }
 }
