efe48e6266ed915319b384ffb12623f1bffc9506
cakn15 <cakn15@student.aau.dk>
2017-10-23 08:51:05 +0200

    Started building testcases for PickingScrape

--- SolarSystem/SolarSystem.Test/Picking and ERP/PickingTest.cs
+++ SolarSystem/SolarSystem.Test/Picking and ERP/PickingTest.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using SolarSystem;
 
 namespace SolarSystem.Test.Picking_and_ERP
 {
     class PickingTest
     {
+        [Theory]
+        [InlineData(101010, 1, "01-01-1001", 10)]
+        public void GetOrdersFromPickingTest(int ordernumber, int articlenumber, int datetime, int quantity)
+        {
+            
+        }
+
     }
 }
