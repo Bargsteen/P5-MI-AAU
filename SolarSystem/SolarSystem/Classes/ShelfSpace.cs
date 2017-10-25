f5b90136efdff3a79283bd07edbcbdd75eaa4d2e
DESKTOP-2IK5FG0\Olive <oliver@trendsofliving.dk>
2017-10-25 08:44:56 +0200

    Implementation of Class diagram done - Added Customer and Shelfspace

---
+++ SolarSystem/SolarSystem/Classes/ShelfSpace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem.Classes
+{
+    public class ShelfSpace
+    {
+        public string Name;
+        public List<ShelfBox> ListOfBoxes;
+
+        public ShelfSpace(string name, List<ShelfBox> listOfBoxes)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            ListOfBoxes = listOfBoxes ?? throw new ArgumentNullException(nameof(listOfBoxes));
+        }
+    }
+}
