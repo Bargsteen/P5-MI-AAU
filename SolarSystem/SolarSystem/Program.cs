8a5d9e77fbfdadc17e51445c4fe72144d823c9a3
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2017-11-01 15:39:45 +0100

    Runner added

--- SolarSystem/SolarSystem/Program.cs
+++ SolarSystem/SolarSystem/Program.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Threading;
 using Ploeh.AutoFixture;
+using SolarSystem.Backend;
 using SolarSystem.Backend.Classes;
 
 
@@ -11,23 +12,24 @@ namespace SolarSystem
     {
         public static void Main(string[] args)
         {
-            var order = OrderHandler.ConstructOrder();
-            var handler = new Handler();
-            handler.OnOrderBoxFinished += box => Console.WriteLine($"BOX FINISHED: {box}");
+
+            var runner = new Runner420();
+            runner.StartSendingOrders();
             
-            TimeKeeper.Tick += () => Console.WriteLine(TimeKeeper.CurrentDateTime);
-            
-            Console.WriteLine("Start ticking");
-            var t = new Thread(() => TimeKeeper.StartTicking(10, DateTime.Now));
-            t.Start();
-            Console.WriteLine("Listen for completed orders");
-            handler.ReceiveOrder(order);
-            
+            /* var order = OrderHandler.ConstructOrder();
+             var handler = new Handler();
+             handler.OnOrderBoxFinished += box => Console.WriteLine($"BOX FINISHED: {box}");
+             
+             TimeKeeper.Tick += () => Console.WriteLine(TimeKeeper.CurrentDateTime);
+             
+             Console.WriteLine("Start ticking");
+             var t = new Thread(() => TimeKeeper.StartTicking(10, DateTime.Now));
+             t.Start();
+             Console.WriteLine("Listen for completed orders");
+             handler.ReceiveOrder(order);*/
+
         }
 
-        public event TickHandler OnTick;
-        
-      
         /*static void Main(string[] args) {
             //var t = new Thread(() => CallToChildThread(1000));
             Console.WriteLine("In Main: Creating the Child thread");
