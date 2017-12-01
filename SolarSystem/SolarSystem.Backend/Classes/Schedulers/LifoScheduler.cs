85341531fbd48de1f49700a494a178d9537ed7c0
Unknown <obpe15@student.aau.dk>
2017-12-01 13:05:36 +0100

    Auto stash before merge of "MiPart" and "origin/MiPart"

---
+++ SolarSystem/SolarSystem.Backend/Classes/Schedulers/LifoScheduler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem.Backend.Classes.Schedulers
+{
+    public class LifoScheduler : SchedulerModular
+    {
+        public LifoScheduler(OrderGenerator orderGenerator, Handler handler, double poolMoverTime) : base(orderGenerator, handler, poolMoverTime)
+        {
+        }
+
+        public override Order ChooseNextOrder()
+        {
+            return ActualOrderPool.OrderBy(o => o.OrderTime).Last();
+        }
+    }
+}
