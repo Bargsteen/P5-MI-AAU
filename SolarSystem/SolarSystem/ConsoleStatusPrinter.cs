41bc0ca3e09750fd74cd028c441e82b4325b597c
cakn15 <cakn15@student.aau.dk>
2017-12-07 13:54:10 +0100

    Added functionality to collect data for when lines are packed and how many at a given time

--- SolarSystem/SolarSystem/ConsoleStatusPrinter.cs
+++ SolarSystem/SolarSystem/ConsoleStatusPrinter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace SolarSystem.Backend.Classes
 {
@@ -30,6 +31,19 @@ namespace SolarSystem.Backend.Classes
             var finishedOrdersPerHour = 0;
             DateTime currentHour = _runner.StartTime;
             var ordersFinishedPerHour = new List<Tuple<int, int>>();
+            Dictionary<Area, List<Tuple<DateTime, int>>> areaLines = new Dictionary<Area, List<Tuple<DateTime, int>>>();
+            Dictionary<AreaCode, double> areaStandartDeviation = new Dictionary<AreaCode, double>()
+            {
+                {AreaCode.Area21, 0},
+                {AreaCode.Area25, 0},
+                {AreaCode.Area27, 0},
+                {AreaCode.Area28, 0},
+                {AreaCode.Area29, 0}
+            };
+            bool firstIteration = true;
+            double average;
+            double sumOfSquaresOfDifferences;
+            double sd;
 
             //runner.Handler.OnOrderBoxFinished += o => PrintStatus($"Handler: Orderbox Finished {o} -- TimeSpend = {o.TimeInSystem}");
 
@@ -38,17 +52,52 @@ namespace SolarSystem.Backend.Classes
                 totalFinishedOrders += orderBox.LineIsPickedStatuses.Keys.Count;
                 finishedOrdersPerHour += orderBox.LineIsPickedStatuses.Keys.Count;
             };
-
+            
             var index = 0;
             TimeKeeper.Tick += () =>
             {
                 if (index++ > 60)
-                    if (TimeKeeper.CurrentDateTime.Hour == currentHour.Hour + 1)
+                    if (TimeKeeper.CurrentDateTime.Hour == currentHour.Hour + 1 || TimeKeeper.CurrentDateTime.Hour == 0)
                     {
                         ordersFinishedPerHour.Add(Tuple.Create(currentHour.Hour, finishedOrdersPerHour));
 
                         currentHour = TimeKeeper.CurrentDateTime;
                         finishedOrdersPerHour = 0;
+
+                        foreach (var _a in areaLines.Keys)
+                        {
+                            //using (StreamWriter dataWriter = new StreamWriter(@"Data/" + _a.Key.ToString() + ".xml"))
+                            using (StreamWriter dataWriter = new StreamWriter("Data/" + _a + ".xml", firstIteration))
+                            {
+                                areaLines[_a].ForEach(x => dataWriter.WriteLine(x.Item1 + ", " + x.Item2));
+                                dataWriter.Close();
+                            }
+
+                            using (StreamWriter dataWriter = new StreamWriter("Data/StandartDeviation" + _a + ".txt", firstIteration))
+                            {
+                                average = 0;
+                                sumOfSquaresOfDifferences = 0;
+                                if (!areaStandartDeviation.ContainsKey(_a.AreaCode))
+                                {
+                                    average = areaLines[_a].Average(v => v.Item2);
+                                    sumOfSquaresOfDifferences = areaLines[_a].Select(val => (val.Item2 - average) * (val.Item2 - average)).Sum();
+                                    sd = Math.Sqrt(sumOfSquaresOfDifferences / areaLines[_a].Count());
+                                    areaStandartDeviation[_a.AreaCode] = sd;
+                                }
+                                else
+                                {
+                                    average = areaLines[_a].Average(v => v.Item2);
+                                    sumOfSquaresOfDifferences = areaLines[_a].Select(val => (val.Item2 - average) * (val.Item2 - average)).Sum();
+                                    sd = Math.Sqrt(sumOfSquaresOfDifferences / areaLines[_a].Count());
+                                    areaStandartDeviation[_a.AreaCode] = ((areaStandartDeviation[_a.AreaCode] + sd) / 2);
+                                }
+
+                                dataWriter.WriteLine("This is the sd: " + areaStandartDeviation[_a.AreaCode]);
+                                dataWriter.Close();
+                            }
+                        }
+                        areaLines.Clear();
+                        firstIteration = false;
                     }
             };
 
@@ -68,6 +117,15 @@ namespace SolarSystem.Backend.Classes
                     Console.WriteLine("Lines between " + TimeKeeper.CurrentDateTime.Hour + " - " +
                                       (TimeKeeper.CurrentDateTime.Hour + 1) + ": " + finishedOrdersPerHour +
                                       " lines");
+
+                    if (areaLines.ContainsKey(area))
+                    {
+                        areaLines[area].Add(new Tuple<DateTime, int>(TimeKeeper.CurrentDateTime, orderBox.LineIsPickedStatuses.Keys.Count()));
+                    }
+                    else
+                    {
+                        areaLines.Add(area, new List<Tuple<DateTime, int>> { new Tuple<DateTime, int>(TimeKeeper.CurrentDateTime, orderBox.LineIsPickedStatuses.Keys.Count()) });
+                    }
                 };
         }
 
